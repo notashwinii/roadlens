0b791e664247ab3439ca64a1725fbbc0f3ce206d
notashwinii <ashwinii.subedi@gmail.com>
2026-06-30 18:41:48 +0545

    refactor: reuse scene geometry helpers

--- core/config_validation.py
+++ core/config_validation.py
@@ -1,26 +1,5 @@
 from core.config_schema import ZoneConfig
-
-
-def normalized_points_to_pixels(
-    points: list[tuple[float, float]],
-    frame_width: int,
-    frame_height: int,
-) -> list[tuple[int, int]]:
-    return [(int(x * frame_width), int(y * frame_height)) for x, y in points]
-
-
-def polygon_to_bounding_rect(
-    points: list[tuple[int, int]],
-) -> tuple[int, int, int, int]:
-    xs = [x for x, _ in points]
-    ys = [y for _, y in points]
-
-    x_min = min(xs)
-    y_min = min(ys)
-    x_max = max(xs)
-    y_max = max(ys)
-
-    return (x_min, y_min, x_max - x_min, y_max - y_min)
+from scene.geometry import normalized_points_to_pixels, polygon_to_bounding_rect
 
 
 def zone_to_roi(
